269ce14372cbd1edea9140be880e25224485c387
Uncanny Forest <git@uncannyforest.com>
2022-12-09 00:37:11 -0500

    Add Express

--- README.md
+++ README.md
@@ -2,10 +2,20 @@
 
 I'm rewriting the website in React with an Express/Postgres backend.  It was originally in Rails.
 
-To run locally:
+To run locally for development:
 ```bash
 npm i
+npm run build
 npm run start
+```
+
+These last two commands should be run in separate terminals.
+
+To run for production:
+```bash
+npm i
+npm run build-prod
+npm run start-prod
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the site.
---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`Server up and running on port ${port}!`));
