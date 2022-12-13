5aee78f0fdb7801bc7a0dc9565f5bbb2fbd28c6b
Uncanny Forest <git@uncannyforest.com>
2022-12-12 21:06:09 -0500

    Don't render comic before backend call

--- client/components/Comics.js
+++ client/components/Comics.js
@@ -15,9 +15,9 @@ class Comics extends React.Component {
     }
   }
 
-  async update() {
+  async update(idString) {
     let response;
-    let id = parseInt(this.props.match.params.id);
+    let id = parseInt(idString);
     if (isNaN(id)) {
       response = await axios.get(`/api/comics/latest`);
       id = response.data.totalComics - 1;
@@ -30,12 +30,14 @@ class Comics extends React.Component {
     })
   }
 
-  componentDidMount() { this.update() };
+  componentDidMount() { this.update(this.props.match.params.id) };
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.match.params.id !== this.props.match.params.id) {
-      this.update();
+  shouldComponentUpdate(nextProps) {
+    if (nextProps.match.params.id !== this.props.match.params.id) {
+      this.update(nextProps.match.params.id);
+      return false;
     }
+    return true;
   };
 
   render() {
@@ -60,5 +62,81 @@ class Comics extends React.Component {
   }
 
 }
+
+var deepDiffMapper = function () {
+  return {
+    VALUE_CREATED: 'created',
+    VALUE_UPDATED: 'updated',
+    VALUE_DELETED: 'deleted',
+    VALUE_UNCHANGED: 'unchanged',
+    map: function(obj1, obj2) {
+      if (this.isFunction(obj1) || this.isFunction(obj2)) {
+        throw 'Invalid argument. Function given, object expected.';
+      }
+      if (this.isValue(obj1) || this.isValue(obj2)) {
+        if (this.compareValues(obj1, obj2) === this.VALUE_UNCHANGED) return null;
+        return {
+          type: this.compareValues(obj1, obj2),
+          data: obj1 === undefined ? obj2 : obj1
+        };
+      }
+
+      var diff = {};
+      for (var key in obj1) {
+        if (this.isFunction(obj1[key])) {
+          continue;
+        }
+
+        var value2 = undefined;
+        if (obj2[key] !== undefined) {
+          value2 = obj2[key];
+        }
+
+        diff[key] = this.map(obj1[key], value2);
+      }
+      for (var key in obj2) {
+        if (this.isFunction(obj2[key]) || diff[key] !== undefined) {
+          continue;
+        }
+
+        diff[key] = this.map(undefined, obj2[key]);
+      }
+
+      return diff;
+
+    },
+    compareValues: function (value1, value2) {
+      if (value1 === value2) {
+        return this.VALUE_UNCHANGED;
+      }
+      if (this.isDate(value1) && this.isDate(value2) && value1.getTime() === value2.getTime()) {
+        return this.VALUE_UNCHANGED;
+      }
+      if (value1 === undefined) {
+        return this.VALUE_CREATED;
+      }
+      if (value2 === undefined) {
+        return this.VALUE_DELETED;
+      }
+      return this.VALUE_UPDATED;
+    },
+    isFunction: function (x) {
+      return Object.prototype.toString.call(x) === '[object Function]';
+    },
+    isArray: function (x) {
+      return Object.prototype.toString.call(x) === '[object Array]';
+    },
+    isDate: function (x) {
+      return Object.prototype.toString.call(x) === '[object Date]';
+    },
+    isObject: function (x) {
+      return Object.prototype.toString.call(x) === '[object Object]';
+    },
+    isValue: function (x) {
+      return !this.isObject(x) && !this.isArray(x);
+    }
+  }
+}();
+
 
 export default Comics;
