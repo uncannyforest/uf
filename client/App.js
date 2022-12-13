cb21ef64c9a8114687ed198ba3c30bfefd1728fe
Uncanny Forest <git@uncannyforest.com>
2022-12-13 00:13:06 -0500

    Add Forest Bio and some cleanup

--- client/App.js
+++ client/App.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { marked } from 'marked';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Comics from './components/Comics';
 import Archive from './components/Archive';
 import About from './components/About';
 import Plains from './components/Plains';
+
+import about from '../config/about.yaml'
 
 class App extends React.Component {
   render() {
@@ -16,6 +19,10 @@ class App extends React.Component {
           <Switch>
             <Route path='/archive' component={Archive} />
             <Route path='/about' component={About} />
+            <Route path='/bio' >
+              <div className='textinfo'
+                dangerouslySetInnerHTML={{__html: marked.parse(about.uf_bio)}} />
+            </Route>
             <Route path='/:id(\d+)' component={Comics} />
             <Route path='/welcome_home' component={Comics} />
             <Route exact path='/' component={Comics} />
--- client/components/About.js
+++ client/components/About.js
@@ -1,27 +1,18 @@
-import axios from 'axios';
 import { marked } from 'marked';
 import React from 'react'
-import { Link } from 'react-router-dom'
-import strftime from 'strftime'
 
 import about from '../../config/about.yaml'
 
-class About extends React.Component {
-  render() {
-    console.log(about);
-
-    return (
-      <div className='textinfo' dangerouslySetInnerHTML={{__html: `
-        ${marked.parse(about.uf)}
-        <h4>ABOUT ME</h4>
-        ${marked.parse(about.me)}
-        <h4>OTHERS</h4>
-        ${marked.parse(about.friends)}
-        <h4>FOLLOW</h4>
-        ${marked.parse(about.follow)}
-      `}} />
-    );
-  }
+const About = () => {
+  return <div className='textinfo' dangerouslySetInnerHTML={{__html: `
+    ${marked.parse(about.uf)}
+    <h4>ABOUT ME</h4>
+    ${marked.parse(about.me)}
+    <h4>OTHERS</h4>
+    ${marked.parse(about.friends)}
+    <h4>FOLLOW</h4>
+    ${marked.parse(about.follow)}
+  `}} />
 }
 
 export default About;
