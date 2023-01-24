30fb35f3ccad4e3a3957aa4ede7f9f846f277787
Uncanny Forest <git@uncannyforest.com>
2023-01-23 21:27:26 -0500

    Clicking 'show comments' scrolls down

--- client/components/Commentary.js
+++ client/components/Commentary.js
@@ -26,7 +26,7 @@ class Commentary extends React.Component {
 
   toggleComments(on) {
     return ((e) => {
-      e.preventDefault();
+      if (!on) e.preventDefault();
       this.setState({ showComments: on })
     }).bind(this)
   }
@@ -36,32 +36,36 @@ class Commentary extends React.Component {
 
     if (noComments && !this.state.showComments || this.state.showComments === false) {
       return (
-        <div className='plains'>
+        <>
           <a name='commentary' />
-          <a href='#' onClick={this.toggleComments(true)}>
-            <div className='commentary-off'>
-              <img src={`/images/commentary-${noComments ? 'write' : 'show'}-2x.png`} className='flashlight-off-msg'/>
-              <img src='/images/flashlight-off-2x.png' className='flashlight-off'/>
-            </div>
-          </a>
-        </div>
+          <div className='plains'>
+            <a href='#commentary' onClick={this.toggleComments(true)}>
+              <div className='commentary-off'>
+                <img src={`/images/commentary-${noComments ? 'write' : 'show'}-2x.png`} className='flashlight-off-msg'/>
+                <img src='/images/flashlight-off-2x.png' className='flashlight-off'/>
+              </div>
+            </a>
+          </div>
+        </>
       )
     }
 
     return (
-      <div className='plains'>
+      <>
         <a name='commentary' />
-        <div className='forest-boundary'>
-          <a href='#' onClick={this.toggleComments(false)}>
-            <img src='/images/flashlight-on-2x.png' className='hf'/>
-          </a>
-          <CommentWriter />
-          <div className='comments'>
-            {this.props.topLevelComments.map(comment => <Comment key={comment.id} data={comment} />)}
+        <div className='plains'>
+          <div className='forest-boundary'>
+            <a href='#' onClick={this.toggleComments(false)}>
+              <img src='/images/flashlight-on-2x.png' className='hf'/>
+            </a>
+            <CommentWriter />
+            <div className='comments'>
+              {this.props.topLevelComments.map(comment => <Comment key={comment.id} data={comment} />)}
+            </div>
+            <img src='/images/grass-floor-2x.png' className='hf' />
           </div>
-          <img src='/images/grass-floor-2x.png' className='hf' />
         </div>
-      </div>
+      </>
     )
   }
 }
