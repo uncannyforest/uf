36ed8dd55346d0aa6ce83f0ebafdbd15dd0b4e49
Uncanny Forest <git@uncannyforest.com>
2023-01-25 22:33:22 -0500

    Add URL to add title to posts in Tumblr tag RSS feeds
    e.g. use uncannyforest.com/rss/blog/photography instead of blog.uncannyforest.com/tagged/photography/rss

--- server/rss.js
+++ server/rss.js
@@ -1,4 +1,7 @@
+const axios = require('axios')
 const express = require('express')
+const xml2js = require('xml2js')
+
 const router = express.Router()
 
 const { desktop, mobile } = require('../lib/imagePaths')
@@ -12,6 +15,35 @@ router.get('/', (req, res) => {
 // GET /rss/mobile
 router.get('/mobile', (req, res) => {
   res.type('application/xml').send(genFeed(true))
+})
+
+// GET /rss/blog/:tag
+router.get('/blog/:tag', async (req, res, next) => {
+  try {
+    const { data } = await axios.get(`https://blog.uncannyforest.com/tagged/${req.params.tag}/rss`)
+    const feed = await xml2js.parseStringPromise(data)
+
+    for (let item of feed.rss.channel[0].item) {
+      let titleFirstPart = item.category[0]
+      let titleSecondPart
+      const description = item.description[0]
+      let pb = description.indexOf('<p><b>') + '<p><b>'.length
+      if (description.indexOf('<p><b>') !== -1 && description.indexOf('</b></p>', pb) !== -1)
+        titleSecondPart = description.substring(pb, description.indexOf('</b></p>', pb))
+      else {
+        const pStart = description.indexOf('<p>') + '<p>'.length
+        const pEnd = description.indexOf('<', pStart)
+        const firstParagraph = description.substring(pStart, pEnd)
+        if (firstParagraph.length <= 48) titleSecondPart = firstParagraph
+        else titleSecondPart = firstParagraph.substring(0, firstParagraph.lastIndexOf(' ', 48)) + ' ...'
+      }
+      item.title = `${titleFirstPart}: ${titleSecondPart}`
+    }
+
+    res.send(new xml2js.Builder().buildObject(feed))
+  } catch (e) {
+    next(e)
+  }
 })
 
 const genFeed = (mobileView) => {
