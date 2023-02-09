2e46792f268cfe649bf26fa429e1b6c22614cfd9
Uncanny Forest <git@uncannyforest.com>
2023-02-09 18:18:39 -0500

    Don't show non-navigatable comic buttons

--- client/components/CompassButton.js
+++ client/components/CompassButton.js
@@ -1,24 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const navAsset = (icon, selected) => {
-  if (selected) return `/images/nav-${icon}-sel-2x.png`
-  else return `/images/nav-${icon}-2x.png`
-}
-
 const CompassButton = ({ empty = false, url, selected, icon }) => {
-  if (empty) return <div className="item"></div>
-
-  if (selected) return (
-    <div className="item">
-      <img src={navAsset(icon, selected)} className='comic-nav' />
-    </div>
-  )
+  if (empty || selected) return <div className="item"></div>
 
   return (
     <div className="item">
       <Link to={url}>
-        <img src={navAsset(icon, selected)} className='comic-nav' />
+        <img src={`/images/nav-${icon}-2x.png`} className='comic-nav' />
       </Link>
     </div>
   )
