b01ca9abe9f71878938ad190a9cefb6f231a7a72
Uncanny Forest <git@uncannyforest.com>
2023-02-10 00:42:16 -0500

    Add Trees & LM to top header

--- client/components/Header.js
+++ client/components/Header.js
@@ -25,14 +25,14 @@ class Header extends React.Component {
       }
     }
 
-    console.log(selectedSection, 'and', selectedSubsection)
-
     return (
       <nav className='header'>
         <img src='/images/canopy-v2-2x.png' className='hf' />
         <ul className='map'>
           <HeaderLink top name='comics' url='/welcome_home'
               selected={selectedSection === 'comics'} />
+          <HeaderLink top name='trees' externalUrl='/blog/tag/trees with faces/latest' />
+          <HeaderLink top name='localmysteries' externalUrl='/blog/tag/local mysteries/latest' />
           <HeaderLink top name='blog' externalUrl='https://blog.uncannyforest.com' />
           <HeaderLink top name='about' url='/about'
               selected={selectedSection === 'about'} />
